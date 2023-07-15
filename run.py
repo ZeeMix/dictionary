5cf23b76be081cbe7cdf216ae6cf89df6766c0b6
Andrey <awesome.foxe@yandex.ru>
2023-07-15 14:43:41 +0300

    Task

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 from user import choose_mode
-from filling_dictionary import filling
+from filling_dictionary import *
+
 
 
 def running():
     person, mode = choose_mode()
     if mode == 'запись':
-        filling(person)
+        fill_person()
+    elif mode == 'поиск':
+        search()        
+    elif mode == 'изменение':
+        modification()
+    elif mode == 'удаление':
+        deletion()
     else:
-        pass
+        print('Неверный режим работы')
 
 
--- user.py
+++ user.py
@@ -1,19 +1,14 @@
+from filling_dictionary import *
+
 def choose_mode():
-    mode = input('Выберите режим для работы со справочником: запись или поиск')
+    mode = input('Выберите режим для работы со справочником: запись, поиск, изменение или удаление: ')
     if mode == 'запись':
         person = fill_person()
     elif mode == 'поиск':
-        pass
+        person = search()
+    elif mode == 'изменение':
+        person = modification()
+    elif mode == 'удаление':
+        person = deletion()
     else:
         print('Такого режима не существует!')
-        choose_mode()
-    return person, mode
-
-
-def fill_person():
-    surname = input('Введите фамилию: ')
-    name = input('Введите имя: ')
-    second_name = input('Введите фамилию: ')
-    phone = input('Введите телефон: ')
-    return surname, name, second_name, phone
-
